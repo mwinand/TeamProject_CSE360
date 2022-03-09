5fa62adf139992e0854170cc7c5b643f34789d29
corytertel <ctertel@comcast.net>
2022-03-08 21:25:17 -0700

    main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 
 
 public class Main {
+    static UserCustomer customer;
+    static UserRestaurant restaurant;
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
