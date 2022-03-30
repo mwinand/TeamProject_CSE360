80ae074ab227ca1381fac586b354f066f3415e56
GGBlaster <98840461+GGBlaster@users.noreply.github.com>
2022-03-30 15:51:27 -0700

    Update User.java
    Added login-related functions to User class

--- src/User.java
+++ src/User.java
@@ -1,5 +1,21 @@
 
 
 public class User {
-    
+    private String username;
+    private String password;
+
+    public void setUsername();
+    public void setPassword();
+    public String getUsername() {
+        return username;
+    }
+    private String getPassword() {
+        return password;
+    }
+    private Boolean checkLoginCredentials(String usernameEntry, String passwordEntry) {
+        Boolean matchingUsername = usernameEntry.equalsIgnoreCase(getUsername());
+        Boolean correctPassword = passwordEntry.equals(getPassword());
+
+        return (matchingUsername && correctPassword);
+    }
 }
